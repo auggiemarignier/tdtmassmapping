171f0e64833de92c062dc621fd48a3b429efab25
Auggie Marignier <augustin.marignier.14@ucl.ac.uk>
2021-04-08 10:39:34 +0100

    use random number generator inherited from GlobalSlice

--- src/massmapping.cpp
+++ src/massmapping.cpp
@@ -5,22 +5,14 @@
 
 int main()
 {
-    // setup random number generator
-    const gsl_rng_type *T;
-    gsl_rng *r;
-    gsl_rng_env_setup();
-    T = gsl_rng_default;
-    r = gsl_rng_alloc(T);
-
     int total = 10;
-
-    for (int i = 0; i < total; i++)
-    {
-        double u = gsl_rng_uniform(r);
-        std::cout << "i=" << i << "\t u=" << u << "\n";
-    }
 
     GlobalSliceMM global("../../../data/Bolshoi_7_clean_256.txt");
 
+    for (int i = 0; i < total; i++)
+    {
+        double u = global.random.uniform();
+        std::cout << "i=" << i << "\t u=" << u << "\n";
+    }
     return 0;
 }
