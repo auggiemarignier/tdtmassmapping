35f9732d77bf9a8c89ae59317a43d5520ae07ef3
Auggie Marignier <augustin.marignier.14@ucl.ac.uk>
2021-04-13 11:51:10 +0100

    fixture and basic test for GlobalSliceMM

--- tests/tests.cpp
+++ tests/tests.cpp
@@ -1,6 +1,25 @@
 #include "gtest/gtest.h"
 
+#include "proposals.hpp"
 #include "mmobservations.hpp"
+
+class GlobalTest : public ::testing::Test
+{
+protected:
+    GlobalSliceMM *global;
+    void SetUp() override
+    {
+        global = new GlobalSliceMM("../../../../data/Bolshoi_7_clean_256.txt");
+    }
+
+    void TearDown() override {
+        delete global;
+    }
+};
+
+TEST_F(GlobalTest, FileReadIn) {
+    ASSERT_TRUE(global->file_read);
+}
 
 TEST(MMObsTest, MMPredsIdentity)
 {
