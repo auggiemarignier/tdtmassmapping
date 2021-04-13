6bb51f86cab8271b2f1c7528854782b55f7182e6
Auggie Marignier <augustin.marignier.14@ucl.ac.uk>
2021-04-13 13:50:35 +0100

    fixture mmobservations, test identity forward modelling

--- tests/tests.cpp
+++ tests/tests.cpp
@@ -12,22 +12,29 @@ protected:
         global = new GlobalSliceMM("../../../../data/Bolshoi_7_clean_256.txt");
     }
 
-    void TearDown() override {
+    void TearDown() override
+    {
         delete global;
     }
 };
 
-TEST_F(GlobalTest, FileReadIn) {
+class MMObsTest : public ::testing::Test
+{
+protected:
+    mmobservations *observations;
+};
+
+TEST_F(GlobalTest, FileReadIn)
+{
     ASSERT_TRUE(global->file_read);
 }
 
-TEST(MMObsTest, MMPredsIdentity)
+TEST_F(MMObsTest, MMPredsIdentity)
 {
-    const double model = 0.0;
-    ASSERT_FALSE(model);
+    ASSERT_FALSE(observations->single_frequency_predictions(nullptr));
 
     const double model1 = 1.0;
-    ASSERT_TRUE(model1);
+    ASSERT_TRUE(observations->single_frequency_predictions(&model1));
 }
 
 int main(int argc, char **argv)
