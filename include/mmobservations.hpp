6778b95061ab66204b4acb1a10863df65f8c6f70
Auggie Marignier <augustin.marignier.14@ucl.ac.uk>
2021-04-27 16:10:36 +0100

    declare non-static variables and additional constructor

--- include/mmobservations.hpp
+++ include/mmobservations.hpp
@@ -11,7 +11,10 @@ public:
     mmobservations(const char *filename);
 
     // Constructor that takes in vectors
-    mmobservations(std::vector<double> obs, std::vector<double> sigma);
+    mmobservations(std::vector<double> _obs, std::vector<double> _sigma);
+
+    // Constructor that takes a vector of obs and the stddev
+    mmobservations(std::vector<double> _obs, double _sigma);
 
     double single_frequency_likelihood(
         std::vector<double> model,
@@ -21,4 +24,8 @@ public:
         double &log_normalization);
 
     std::vector<double> single_frequency_predictions(std::vector<double> model);
+
+    std::vector<double> obs;
+    double sigma;
+    size_t n_obs;
 };
