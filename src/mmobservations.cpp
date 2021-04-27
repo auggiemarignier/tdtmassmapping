037f4bf7f15a2010141c61a47471c17fd5ab43a7
Auggie Marignier <augustin.marignier.14@ucl.ac.uk>
2021-04-27 14:36:20 +0100

    setup likelihood function

--- src/mmobservations.cpp
+++ src/mmobservations.cpp
@@ -6,8 +6,18 @@ extern "C"
 #include "slog.h"
 };
 
-std::vector<double> mmobservations::single_frequency_predictions(std::vector<double> model)
+std::vector<double> mmobservations::single_frequency_predictions(
+    std::vector<double> model)
 {
     std::vector<double> predictions = model;
     return predictions;
+}
+
+double mmobservations::single_frequency_likelihood(
+    std::vector<double> model,
+    const hierarchicalmodel *hmodel,
+    double *residuals,
+    double *residuals_normed,
+    double &log_normalization)
+{
 }
