219afcd78ecb66d7efee8f522d5358fab88888ff
Auggie Marignier <augustin.marignier.14@ucl.ac.uk>
2021-04-29 15:50:20 +0100

    split up tests into separate files

--- tests/test_mmobservations.cpp
+++ tests/test_mmobservations.cpp
@@ -1,28 +1,7 @@
 #include "gtest/gtest.h"
 #include <vector>
 
-#include "proposals.hpp"
 #include "mmobservations.hpp"
-
-class GlobalTest : public ::testing::Test
-{
-protected:
-    GlobalSliceMM *global;
-    void SetUp() override
-    {
-        global = new GlobalSliceMM("../../data/Bolshoi_7_clean_256.txt");
-    }
-
-    void TearDown() override
-    {
-        delete global;
-    }
-};
-
-TEST_F(GlobalTest, GlobalSetup)
-{
-    ASSERT_FLOAT_EQ(global->stddev, 0.034364982263599415);
-}
 
 class MMObsTest : public ::testing::Test
 {
