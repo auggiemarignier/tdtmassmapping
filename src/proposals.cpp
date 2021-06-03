dad6d2cbc56d4ed65f031fdc3e327ff015d1b951
Auggie Marignier <augustin.marignier.14@ucl.ac.uk>
2021-06-03 11:20:38 +0100

    copy in methods that don't depend on proposal type at all

--- src/proposals.cpp
+++ src/proposals.cpp
@@ -21,3 +21,44 @@ Proposal::Proposal(GlobalProposal &_global)
         accept_depth[i] = 0;
     }
 }
+
+std::string Proposal::write_short_stats()
+{
+    return mkformatstring("Birth %6d/%6d %7.3f",
+                          accept,
+                          propose,
+                          propose == 0 ? 0.0 : 100.0 * (double)accept / (double)propose);
+}
+
+std::string Proposal::write_long_stats()
+{
+    std::string s = mkformatstring("Birth: %6d %7.3f:",
+                                   propose,
+                                   propose == 0 ? 0.0 : 100.0 * (double)accept / (double)propose);
+    for (int i = 0; i <= global.treemaxdepth; i++)
+    {
+        s = s + mkformatstring("%7.3f ",
+                               propose_depth[i] == 0 ? 0.0 : 100.0 * (double)accept_depth[i] / (double)propose_depth[i]);
+    }
+
+    return s;
+}
+
+void Proposal::initialize_mpi(MPI_Comm _communicator)
+{
+    MPI_Comm_dup(_communicator, &communicator);
+
+    if (MPI_Comm_size(communicator, &mpi_size) != MPI_SUCCESS)
+    {
+        throw WAVETOMO2DEXCEPTION("MPI Failure\n");
+    }
+    if (MPI_Comm_rank(communicator, &mpi_rank) != MPI_SUCCESS)
+    {
+        throw WAVETOMO2DEXCEPTION("MPI Failure\n");
+    }
+}
+
+bool Proposal::primary() const
+{
+    return (communicator == MPI_COMM_NULL || mpi_rank == 0);
+}
