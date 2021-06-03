f6cc2d00a35f1f0e3af71bd68544fb425fea8d30
Auggie Marignier <augustin.marignier.14@ucl.ac.uk>
2021-06-03 17:10:27 +0100

    test birth step

--- tests/test_proposals.cpp
+++ tests/test_proposals.cpp
@@ -34,3 +34,37 @@ TEST_F(ProposalTest, NameEnum)
 {
     ASSERT_EQ(proposal->name, 0);
 }
+
+class BirthTest : public ::testing::Test
+{
+protected:
+    mmobservations *observations;
+    GlobalProposal *global;
+    BirthProposal *birth;
+    void SetUp() override
+    {
+        std::vector<double> obs = {1, 2, 3, 4, 5};
+        double sigma = 1.41;
+        observations = new mmobservations(obs, sigma);
+
+        global = new GlobalProposal(NULL,
+                                    observations,
+                                    NULL,
+                                    "tutorial_prior.txt",
+                                    8,
+                                    8,
+                                    1,
+                                    100,
+                                    4);
+
+        birth = new BirthProposal(*global);
+    }
+};
+
+TEST_F(BirthTest, BirthStep)
+{
+    ASSERT_EQ(birth->name, 1);
+    int step_taken = birth->step();
+    ASSERT_EQ(birth->propose, 1);
+    ASSERT_TRUE(step_taken == 0 || step_taken == 1);
+}
