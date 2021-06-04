e6ed0e49efb40641f995e006682b2340d5c40938
Auggie Marignier <augustin.marignier.14@ucl.ac.uk>
2021-06-04 15:15:55 +0100

    set up simple test for death proposal

--- tests/test_proposals.cpp
+++ tests/test_proposals.cpp
@@ -68,3 +68,37 @@ TEST_F(BirthTest, BirthStep)
     ASSERT_EQ(birth->propose, 1);
     ASSERT_TRUE(step_taken == 0 || step_taken == 1);
 }
+
+class DeathTest : public ::testing::Test
+{
+protected:
+    mmobservations *observations;
+    GlobalProposal *global;
+    DeathProposal *death;
+    void SetUp() override
+    {
+        std::vector<double> obs = {1, 2, 3, 4, 5};
+        double sigma = 1.41;
+        observations = new mmobservations(obs, sigma);
+
+        global = new GlobalProposal(NULL,
+                                    observations,
+                                    NULL,
+                                    "tutorial_prior.txt",
+                                    8,
+                                    8,
+                                    1,
+                                    100,
+                                    4);
+
+        death = new DeathProposal(*global);
+    }
+};
+
+TEST_F(DeathTest, DeathStep)
+{
+    ASSERT_EQ(death->name, 2);
+    int step_taken = death->step();
+    ASSERT_EQ(death->propose, 1);
+    ASSERT_TRUE(step_taken == 0 || step_taken == 1);
+}
