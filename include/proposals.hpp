6a746b5682abb5c60c025f7edb4e7d1184b6b725
Auggie Marignier <augustin.marignier.14@ucl.ac.uk>
2021-06-04 12:06:56 +0100

    move to utils to avoid duplicate symbols

--- include/proposals.hpp
+++ include/proposals.hpp
@@ -4,31 +4,6 @@
 
 #include <mpi.h>
 
-std::string enum_to_string(wavetree_perturb_t type)
-{
-    switch (type)
-    {
-    case WT_PERTURB_INVALID:
-        return "Invalid";
-    case WT_PERTURB_NONE:
-        return "None";
-    case WT_PERTURB_BIRTH:
-        return "Birth";
-    case WT_PERTURB_DEATH:
-        return "Death";
-    case WT_PERTURB_VALUE:
-        return "Value";
-    case WT_PERTURB_MOVE:
-        return "Move";
-    case WT_PERTURB_HIERARCHICAL:
-        return "Hierarchical";
-    case WT_PERTURB_PTEXCHANGE:
-        return "PT Exchange";
-    case WT_PERTURB_PTMODELEXCHANGE:
-        return "PT Model Exchange";
-    }
-}
-
 class Proposal
 {
 public:
