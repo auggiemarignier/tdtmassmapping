3f2e92e75e7acf3c3031ed8b9721ebee3a900097
Auggie Marignier <augustin.marignier.14@ucl.ac.uk>
2021-07-04 13:14:56 +0100

    generate the dot string

--- plotting/wavetree.py
+++ plotting/wavetree.py
@@ -2,6 +2,7 @@ import networkx as nx
 from networkx.drawing.nx_pydot import from_pydot
 import matplotlib.pyplot as plt
 import pydot
+import numpy as np
 
 
 def index_to_2d(index, width):
@@ -15,7 +16,7 @@ def index_from_2d(ii, ij, width):
 
 
 def parent_index(index, width):
-    if index >= width**2 or index < 0:
+    if index >= width ** 2 or index < 0:
         raise ValueError()
     if index == 0:
         return 0
@@ -29,57 +30,37 @@ def depth_from_index(index, width):
     return 1 + depth_from_index(parent_index(index, width), width) if index != 0 else 0
 
 
-dot_string = """graph my_graph {
-    a -- b1 -- c1 -- d1;
-    a -- b1 -- c1 -- d2;
-    a -- b1 -- c1 -- d3;
-    a -- b1 -- c1 -- d4;
-    a -- b1 -- c2 -- d5;
-    a -- b1 -- c2 -- d6;
-    a -- b1 -- c2 -- d7;
-    a -- b1 -- c2 -- d8;
-    a -- b1 -- c3 -- d9;
-    a -- b1 -- c3 -- d10;
-    a -- b1 -- c3 -- d11;
-    a -- b1 -- c3 -- d12;
-    a -- b1 -- c4 -- d13;
-    a -- b1 -- c4 -- d14;
-    a -- b1 -- c4 -- d15;
-    a -- b1 -- c4 -- d16;
-    a -- b2 -- c5 -- d17;
-    a -- b2 -- c5 -- d18;
-    a -- b2 -- c5 -- d19;
-    a -- b2 -- c5 -- d20;
-    a -- b2 -- c6 -- d21;
-    a -- b2 -- c6 -- d22;
-    a -- b2 -- c6 -- d23;
-    a -- b2 -- c6 -- d24;
-    a -- b2 -- c7 -- d25;
-    a -- b2 -- c7 -- d26;
-    a -- b2 -- c7 -- d27;
-    a -- b2 -- c7 -- d28;
-    a -- b2 -- c8 -- d29;
-    a -- b2 -- c8 -- d30;
-    a -- b2 -- c8 -- d31;
-    a -- b2 -- c8 -- d32;
-    a -- b3 -- c9 -- d33;
-    a -- b3 -- c9 -- d34;
-    a -- b3 -- c9 -- d35;
-    a -- b3 -- c9 -- d36;
-    a -- b3 -- c10 -- d37;
-    a -- b3 -- c10 -- d38;
-    a -- b3 -- c10 -- d39;
-    a -- b3 -- c10 -- d40;
-    a -- b3 -- c11 -- d41;
-    a -- b3 -- c11 -- d42;
-    a -- b3 -- c11 -- d43;
-    a -- b3 -- c11 -- d44;
-    a -- b3 -- c12 -- d45;
-    a -- b3 -- c12 -- d46;
-    a -- b3 -- c12 -- d47;
-    a -- b3 -- c12 -- d48;
-}
-"""
+def branch_string(index, width, depth):
+    branch = np.zeros(depth + 1, dtype=int)
+    branch[depth] = index
+    current = index
+    for dpt in range(depth - 1, -1, -1):
+        parent = parent_index(current, width)
+        branch[dpt] = parent
+        current = parent
+    return " -- ".join(branch.astype(str))
+
+
+def tree_string(width, maxdepth):
+    assert maxdepth <= np.log2(width)
+    strings = []
+    for index in range(width**2):
+        depth = depth_from_index(index, width)
+        if depth == maxdepth:
+            strings.append(branch_string(index, width, maxdepth))
+    return "\n".join(strings)
+
+
+def generate_dot_string(width, depth):
+    return f"graph my_graph {{{tree_string(width, depth)}}}"
+
+
+img_width = 256
+max_depth = 3
+dot_string = generate_dot_string(img_width, max_depth)
+
+wavs_arr = np.loadtxt("outputs/wavelets.txt", skiprows=899)
+
 
 graphs = pydot.graph_from_dot_data(dot_string)
 graph = graphs[0]
@@ -89,23 +70,10 @@ G = from_pydot(graph)
 pos = nx.kamada_kawai_layout(G)
 
 common_options = {"node_size": 100}
-nx.draw(G, pos, **common_options)
-nx.draw_networkx_nodes(G, pos, nodelist=["a"], node_color="red", **common_options)
+# nx.draw(G, pos, node_color="white", **common_options)
+nx.draw_networkx_nodes(G, pos, nodelist=["0"], node_color="red", **common_options)
 nx.draw_networkx_nodes(
-    G, pos, nodelist=["b1", "b2", "b3"], node_color="blue", **common_options
+    G, pos, nodelist=["1", "256", "257"], node_color="blue", **common_options
 )
-nx.draw_networkx_nodes(
-    G,
-    pos,
-    nodelist=[f"c{i}" for i in range(1, 13)],
-    node_color="green",
-    **common_options,
-)
-nx.draw_networkx_nodes(
-    G,
-    pos,
-    nodelist=[f"d{i}" for i in range(1, 49)],
-    node_color="yellow",
-    **common_options,
-)
+nx.draw_networkx_nodes(G, pos, node_color=wavs_arr, **common_options)
 plt.show()
