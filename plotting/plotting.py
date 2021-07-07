ea1992c04d8728740b53c367042d26574fff039f
Auggie Marignier <augustin.marignier.14@ucl.ac.uk>
2021-07-07 11:23:38 +0100

    add stddev map

--- plotting/plotting.py
+++ plotting/plotting.py
@@ -8,18 +8,23 @@ import sys
 directory = sys.argv[1]
 truth = np.loadtxt("../data/Bolshoi_7_clean_256.txt").reshape((256, 256))
 mean = np.loadtxt(f"{directory}/mean.txt")
+std = np.loadtxt(f"{directory}/stddev.txt")
 
 vmin = truth.min()
 vmax = truth.max()
+stdmin = std.min()
+stdmax = std.max()
 
 cmap = cm.inferno
+diffcmap = cm.binary
+stdcmap = cm.turbo
 
-mosaic = """ABC
-            D.."""
-fig = plt.figure(figsize=(10, 5), constrained_layout=True)
+mosaic = """ABE
+            CDF"""
+fig = plt.figure(figsize=(8, 8), tight_layout=True)
 axd = fig.subplot_mosaic(
     mosaic,
-    gridspec_kw={"height_ratios": [15, 1], "wspace": 0.001},
+    gridspec_kw={"width_ratios": [15, 15, 1], "wspace": 0.05},
 )
 
 axd["A"].imshow(truth, vmin=vmin, vmax=vmax, cmap=cmap)
@@ -30,19 +35,24 @@ axd["B"].imshow(mean, vmin=vmin, vmax=vmax, cmap=cmap)
 axd["B"].set_title("Mean TDT")
 axd["B"].axis("off")
 
-axd["C"].imshow(truth - mean, cmap="binary")
+axd["C"].imshow(truth - mean, cmap=diffcmap)
 axd["C"].set_title("Truth - Mean")
 axd["C"].axis("off")
 
+axd["D"].imshow(std, cmap=stdcmap)
+axd["D"].set_title("Standard Dev.")
+axd["D"].axis("off")
+
 fig.colorbar(
     cm.ScalarMappable(norm=Normalize(vmin=vmin, vmax=vmax), cmap=cmap),
-    axd["D"],
-    orientation="horizontal",
+    axd["E"],
     shrink=0.1,
 )
-axd["D"].axis("off")
-l, b, w, h = axd["D"].get_position(original=False).bounds
-axd["D"].set_position([l + 0.15, 0.01, w, h])
+fig.colorbar(
+    cm.ScalarMappable(norm=Normalize(vmin=stdmin, vmax=stdmax), cmap=stdcmap),
+    axd["F"],
+    shrink=0.1,
+)
 
 plt.show()
 
