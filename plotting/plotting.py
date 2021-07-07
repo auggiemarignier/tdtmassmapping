c7e9dd0a5ce7c0c4edfd0dddf333cc622f294991
Auggie Marignier <augustin.marignier.14@ucl.ac.uk>
2021-07-07 16:23:54 +0100

    add colourbar to difference map

--- plotting/plotting.py
+++ plotting/plotting.py
@@ -14,7 +14,9 @@ khist = np.loadtxt(f"{directory}/khistogram.txt")
 last_nonzero_k = np.argwhere(khist[:, 1]).max()
 likelihoods = np.loadtxt(f"{directory}/likelihood.txt")
 
+restarts = []
 while os.path.isdir(f"{directory}/restart/"):
+    restarts.append(likelihoods.shape[0])
     directory += "/restart"
     if os.path.isfile(f"{directory}/mean.txt"):
         mean += np.loadtxt(f"{directory}/mean.txt")
@@ -23,21 +25,25 @@ while os.path.isdir(f"{directory}/restart/"):
         khist[:, 1] += np.loadtxt(f"{directory}/khistogram.txt", usecols=1)
         last_nonzero_k = np.argwhere(khist[:, 1]).max()
 
+diff = np.abs(truth - mean)
+
 vmin = truth.min()
 vmax = truth.max()
 stdmin = std.min()
 stdmax = std.max()
+diffmin = diff.min()
+diffmax = diff.max()
 
 cmap = cm.inferno
-diffcmap = cm.binary
+diffcmap = cm.binary_r
 stdcmap = cm.turbo
 
-mosaic = """ABE
-            CDF"""
-fig = plt.figure(figsize=(8, 8), tight_layout=True)
+mosaic = """.ABE
+            GCDF"""
+fig = plt.figure(figsize=(8, 8))
 axd = fig.subplot_mosaic(
     mosaic,
-    gridspec_kw={"width_ratios": [15, 15, 1], "wspace": 0.05},
+    gridspec_kw={"width_ratios": [1, 15, 15, 1], "wspace": 0.05},
 )
 
 axd["A"].imshow(truth, vmin=vmin, vmax=vmax, cmap=cmap)
@@ -48,8 +54,8 @@ axd["B"].imshow(mean, vmin=vmin, vmax=vmax, cmap=cmap)
 axd["B"].set_title("Mean TDT")
 axd["B"].axis("off")
 
-axd["C"].imshow(truth - mean, cmap=diffcmap)
-axd["C"].set_title("Truth - Mean")
+axd["C"].imshow(diff, cmap=diffcmap)
+axd["C"].set_title("|Truth - Mean|")
 axd["C"].axis("off")
 
 axd["D"].imshow(std, cmap=stdcmap)
@@ -66,6 +72,12 @@ fig.colorbar(
     axd["F"],
     shrink=0.1,
 )
+fig.colorbar(
+    cm.ScalarMappable(norm=Normalize(vmin=diffmin, vmax=diffmax), cmap=diffcmap),
+    axd["G"],
+    shrink=0.1,
+)
+axd["G"].yaxis.set_ticks_position('left')
 
 plt.show()
 
@@ -79,6 +91,8 @@ axd["A"].set_xlabel("Number of parameters")
 axd["A"].set_ylabel("Count")
 
 axd["B"].plot(likelihoods)
+for r in restarts:
+    axd["B"].axvline(r, c="k", ls="--")
 axd["B"].set_yscale("log")
 axd["B"].set_xlabel("Sample number")
 axd["B"].set_ylabel("-log(likelihood)")
