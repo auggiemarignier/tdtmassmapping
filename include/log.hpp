01f75ba5e7f5c07b67466a02f96ee18bc4158693
Auggie Marignier <augustin.marignier.14@ucl.ac.uk>
2021-07-26 16:42:25 +0100

    print less stuff for INFO and WARNING

--- include/log.hpp
+++ include/log.hpp
@@ -26,6 +26,6 @@ extern "C"
 
 
 #define ERROR(fmt, ...) slog(SLOG_ERROR, __FILENAME__, __FUNCTION__, __LINE__, fmt, ##__VA_ARGS__)
-#define WARNING(fmt, ...) slog(SLOG_WARNING, __FILENAME__, __FUNCTION__, __LINE__, fmt, ##__VA_ARGS__)
-#define INFO(fmt, ...) slog(SLOG_INFO, __FILENAME__, __FUNCTION__, __LINE__, fmt, ##__VA_ARGS__)
-#define DEBUG(fmt, ...) slog(SLOG_WARNING, __FILENAME__, __FUNCTION__, __LINE__, fmt, ##__VA_ARGS__)
+#define WARNING(fmt, ...) slog(SLOG_WARNING, "", "", 0, fmt, ##__VA_ARGS__)
+#define INFO(fmt, ...) slog(SLOG_INFO, "", "", 0, fmt, ##__VA_ARGS__)
+#define DEBUG(fmt, ...) slog(SLOG_DEBUG, __FILENAME__, __FUNCTION__, __LINE__, fmt, ##__VA_ARGS__)
