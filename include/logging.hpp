ea25c9a6f4e54500178df69c66203295ee43c780
Auggie Marignier <augustin.marignier.14@ucl.ac.uk>
2021-08-01 10:07:05 +0100

    start logging struct

--- include/logging.hpp
+++ include/logging.hpp
@@ -29,3 +29,23 @@ extern "C"
 #define WARNING(fmt, ...) slog(SLOG_WARNING, "", "", 0, fmt, ##__VA_ARGS__)
 #define INFO(fmt, ...) slog(SLOG_INFO, "", "", 0, fmt, ##__VA_ARGS__)
 #define DEBUG(fmt, ...) slog(SLOG_DEBUG, __FILENAME__, __FUNCTION__, __LINE__, fmt, ##__VA_ARGS__)
+
+typedef enum
+{
+    ERROR = 0,
+    WARNING = 1,
+    INFO = 3,
+    DEBUG = 4
+} logger_level_t;
+
+struct Logger
+{
+    Logger();
+
+    FILE open_log_file();
+    void write_log(logger_level_t level, const char *fmt, ...);
+    void close_log();
+
+    const char *filename;
+    FILE *log_file;
+};
