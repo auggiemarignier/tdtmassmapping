c802f3ef813097ef9a5a1539925d7664860115f9
Auggie Marignier <augustin.marignier.14@ucl.ac.uk>
2021-08-04 13:53:50 +0100

    remove old logging macros

--- include/logging.hpp
+++ include/logging.hpp
@@ -1,37 +1,5 @@
 #include <string>
 
-
-#if 0 
-extern "C"
-{
-#include "slog.h"
-};
-
-#ifdef ERROR
-#undef ERROR
-#endif
-
-#ifdef WARNING
-#undef WARNING
-#endif
-
-#ifdef INFO
-#undef INFO
-#endif
-
-#ifdef DEBUG
-#undef DEBUG
-#endif
-
-
-
-
-#define ERROR(fmt, ...) slog(SLOG_ERROR, __FILENAME__, __FUNCTION__, __LINE__, fmt, ##__VA_ARGS__)
-#define WARNING(fmt, ...) slog(SLOG_WARNING, "", "", 0, fmt, ##__VA_ARGS__)
-#define INFO(fmt, ...) slog(SLOG_INFO, "", "", 0, fmt, ##__VA_ARGS__)
-#define DEBUG(fmt, ...) slog(SLOG_DEBUG, __FILENAME__, __FUNCTION__, __LINE__, fmt, ##__VA_ARGS__)
-#endif
-
 #define __FILENAME__ (strrchr(__FILE__, '/') ? strrchr(__FILE__, '/') + 1 : __FILE__)
 
 #define ERROR(logger, fmt, ...) logger.write_log(ERROR, fmt, ##__VA_ARGS__)
