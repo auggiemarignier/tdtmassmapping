7cebb17101e0c37c326e2f656e0b2a7b53d58ac0
Auggie Marignier <augustin.marignier.14@ucl.ac.uk>
2021-08-06 14:53:33 +0100

    don't link the log lib from tdtbase

--- cmake/GetTDTBase.cmake
+++ cmake/GetTDTBase.cmake
@@ -12,7 +12,6 @@ ExternalProject_Get_property(${TDTBASE} SOURCE_DIR)
 set(TDTBASE_SRC ${SOURCE_DIR})
 list(APPEND TDTBASE_INCLUDE
     ${TDTBASE_SRC}/hnk
-    ${TDTBASE_SRC}/log
     ${TDTBASE_SRC}/oset
     ${TDTBASE_SRC}/sphericalwavelet
     ${TDTBASE_SRC}/tracking
@@ -21,11 +20,9 @@ list(APPEND TDTBASE_INCLUDE
     )
 list(APPEND TDTBASE_LIBS
     ${TDTBASE_SRC}/hnk/libhnk.a
-    ${TDTBASE_SRC}/log/liblog.a
     ${TDTBASE_SRC}/oset/liboset.a
     ${TDTBASE_SRC}/sphericalwavelet/libsphericalwavelet.a
     ${TDTBASE_SRC}/tracking/libtracking.a
     ${TDTBASE_SRC}/wavelet/libwavelet.a
     ${TDTBASE_SRC}/wavetree/libwavetree.a
-
 )
