42231480686b63ea1bd7b70a16518b93ba43374f
Auggie Marignier <augustin.marignier.14@ucl.ac.uk>
2021-09-03 13:52:34 +0100

    remove spherical stuff

--- include/measurement_operators.hpp
+++ include/measurement_operators.hpp
@@ -12,35 +12,6 @@
 
 namespace darkmapper {
 
-//! Kernels related to functions on the sphere.
-namespace spherical_kernels {
-//! Covariance weighting applied only to the masked coefficients.
-std::tuple<sopt::OperatorFunction<Vector<t_complex>>, sopt::OperatorFunction<Vector<t_complex>>> 
-covariance_weighting(const t_int& L, const Vector<t_real>& covariance, const t_int& Size_Mask);
-//! Masking operator 
-std::tuple<sopt::OperatorFunction<Vector<t_complex>>, sopt::OperatorFunction<Vector<t_complex>>> 
-masking(const t_int& L, const Vector<t_int>& mask);
-//! Applying mask to complex vectors.
-Vector<t_complex> apply_mask(const Vector<t_complex>& x, const Vector<t_int>& mask);
-//! Applying mask to real vectors.
-Vector<t_real> apply_mask_r(const Vector<t_real>& x, const Vector<t_int>& mask);
-//! Computes harmonic space mapping kernel
-Vector<t_complex> harmonic_space_kernel(const t_int& L);
-//! Constructs lambda functions for applying kernel in harmonic space.
-std::tuple<sopt::OperatorFunction<Vector<t_complex>>, sopt::OperatorFunction<Vector<t_complex>>> 
-harmonic_mapping(const t_int& L);
-}   // namespace spherical_kernels
-
-//! operator that will perform spherical mass-mapping inversion.
-namespace spherical_model {
-//! Construct linear operator for masked analysis darkmapper_m mass-mapping  algorithm.
-std::shared_ptr<sopt::LinearTransform<Vector<t_complex>>> 
-darkmapperSphere(const t_int& L, const Vector<t_real>& covariance, const Vector<t_int>& mask, 
-            const t_int& ssht_verbose, const ssht_dl_method_t dl_method);
-//! Constructs linear operator for Spherical Kaiser Squires mass-mapping.
-std::shared_ptr<sopt::LinearTransform<Vector<t_complex>>>
-spherical_kaiser_squires(const t_int& L, const t_int& ssht_verbose, const ssht_dl_method_t dl_method);
-}   // namespace spherical_model
 
 //! operator that will perform planar mass-mapping inversion.
 namespace planar_model {
