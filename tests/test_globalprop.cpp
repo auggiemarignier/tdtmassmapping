76ff0fb4fbb8cdf7be911a8d92e6855d1068654a
Auggie Marignier <augustin.marignier.14@ucl.ac.uk>
2021-09-23 13:17:57 +0100

    remove test that just checked residuals

--- tests/test_globalprop.cpp
+++ tests/test_globalprop.cpp
@@ -6,13 +6,13 @@
 class GlobalPropTest : public ::testing::Test
 {
 protected:
-    mmobservations *observations;
+    Identity *observations;
     GlobalProposal *global;
     void SetUp() override
     {
         std::vector<double> obs = {1, 2, 3, 4, 5};
         double sigma = 1.41;
-        observations = new mmobservations(obs, sigma);
+        observations = new Identity();
 
         global = new GlobalProposal(observations,
                                     NULL,
@@ -35,24 +35,4 @@ TEST_F(GlobalPropTest, GlobalLikelihood)
     // wavetree model at initialisation is 0 everywhere
     double likelihood = global->likelihood(global->current_log_normalization);
     ASSERT_FLOAT_EQ(likelihood, 13.8323);
-}
-
-TEST_F(GlobalPropTest, GlobalAccept)
-{
-    ASSERT_EQ(global->mean_residual_n, 0);
-    for (int i = 0; i < global->residual_size; i++)
-    {
-        ASSERT_EQ(global->mean_residual[i], 0);
-        ASSERT_EQ(global->last_valid_residual[i], 0);
-    }
-
-    double likelihood = global->likelihood(global->current_log_normalization);
-    global->accept();
-
-    ASSERT_EQ(global->mean_residual_n, 1);
-    for (int i = 0; i < global->residual_size; i++)
-    {
-        ASSERT_EQ(global->mean_residual[i], global->observations->obs[i]);
-        ASSERT_EQ(global->last_valid_residual[i], global->observations->obs[i]);
-    }
 }
