8945780b989b032dd56fb8bf718a9c995bbef298
Auggie Marignier <augustin.marignier.14@ucl.ac.uk>
2021-09-24 18:53:49 +0100

    no leak and faster when using kaiser_squires

--- src/leak.cpp
+++ src/leak.cpp
@@ -8,9 +8,12 @@ int main()
     Rng random(1);
 
     complexvector input;
+    fftw_complex *inputfftw = (fftw_complex *)fftw_malloc(sizeof(fftw_complex) * imsize);
     for (uint j = 0; j < imsize; j++)
     {
         input.emplace_back(random.normal(1.), random.normal(1.));
+        inputfftw[j][0] = input[j].real();
+        inputfftw[j][1] = input[j].imag();
     }
     mmobservations observations(32, 32);
     Logger::open_log(0);
@@ -18,7 +21,9 @@ int main()
     int i = 1;
     while (i < 1e6)
     {
-        observations.single_frequency_predictions(input);
+        fftw_complex *outputfftw = (fftw_complex *)fftw_malloc(sizeof(fftw_complex) * imsize);
+        observations.kaiser_squires(outputfftw, inputfftw);
+        fftw_free(outputfftw);
         i++;
         if (i % 10000 == 0)
             INFO("%i iterations", i);
