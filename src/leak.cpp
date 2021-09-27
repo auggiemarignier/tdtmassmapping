478a7ebb2fe57ef65ccf03583b8da59c9790cbe3
Auggie Marignier <augustin.marignier.14@ucl.ac.uk>
2021-09-27 10:52:40 +0100

    no more leak

--- src/leak.cpp
+++ src/leak.cpp
@@ -8,12 +8,9 @@ int main()
     Rng random(1);
 
     complexvector input;
-    fftw_complex *inputfftw = (fftw_complex *)fftw_malloc(sizeof(fftw_complex) * imsize);
     for (uint j = 0; j < imsize; j++)
     {
         input.emplace_back(random.normal(1.), random.normal(1.));
-        inputfftw[j][0] = input[j].real();
-        inputfftw[j][1] = input[j].imag();
     }
     mmobservations observations(32, 32);
     Logger::open_log(0);
@@ -21,9 +18,7 @@ int main()
     int i = 1;
     while (i < 1e6)
     {
-        fftw_complex *outputfftw = (fftw_complex *)fftw_malloc(sizeof(fftw_complex) * imsize);
-        observations.kaiser_squires(outputfftw, inputfftw);
-        fftw_free(outputfftw);
+        observations.single_frequency_predictions(input);
         i++;
         if (i % 10000 == 0)
             INFO("%i iterations", i);
