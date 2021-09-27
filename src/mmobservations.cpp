3f381b46bce7a3ddca4fa33ee50f26c9b75dcb91
Auggie Marignier <augustin.marignier.14@ucl.ac.uk>
2021-09-27 10:48:49 +0100

    rewrite predictions function

--- src/mmobservations.cpp
+++ src/mmobservations.cpp
@@ -124,23 +124,11 @@ mmobservations::mmobservations(const uint _imsizex, const uint _imsizey)
     Dadj = std::get<2>(operator_tuple);
 };
 
-complexvector mmobservations::single_frequency_predictions(complexvector &model)
+complexvector mmobservations::single_frequency_predictions(complexvector &kappa)
 {
-    fftw_complex *kappa = (fftw_complex *)fftw_malloc(sizeof(fftw_complex) * imsize);
-    fftw_complex *gamma = (fftw_complex *)fftw_malloc(sizeof(fftw_complex) * imsize);
-    for (uint i = 0; i < imsize; i++)
-    {
-        kappa[i][0] = model[i].real();
-        kappa[i][1] = model[i].imag();
-    }
+    complexvector gamma(imsize);
     kaiser_squires(gamma, kappa);
-    complexvector predictions;
-    predictions.reserve(imsize);
-    for (uint i = 0; i < imsize; i++)
-        predictions.emplace_back(gamma[i][0], gamma[i][1]);
-    return predictions;
-    fftw_free(kappa);
-    fftw_free(gamma);
+    return gamma;
 }
 
 std::tuple<std::function<void(complexvector &, const complexvector &)>, std::function<void(complexvector &, const complexvector &)>> mmobservations::init_fft_2d()
