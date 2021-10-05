3255d0c439c07cc8f75bd3559aefeab988772762
Auggie Marignier <augustin.marignier.14@ucl.ac.uk>
2021-10-05 14:41:31 +0100

    test snr

--- tests/test_utils.cpp
+++ tests/test_utils.cpp
@@ -1,38 +1,57 @@
 #include "gtest/gtest.h"
 
 #include "utils.hpp"
+#include "rng.hpp"
 
-TEST(UtilsTest, TestMeanComplex)
+TEST(UtilsTest, MeanComplex)
 {
     std::vector<std::complex<double>> vec(5);
     for (int i = 1; i <= vec.size(); i++)
-        vec[i-1] = std::complex<double>(i, vec.size() - i);
+        vec[i - 1] = std::complex<double>(i, vec.size() - i);
     auto mean = vector_mean(vec);
     ASSERT_FLOAT_EQ(mean.real(), 3.);
     ASSERT_FLOAT_EQ(mean.imag(), 2.);
 };
 
-TEST(UtilsTest, TestMeanDouble)
+TEST(UtilsTest, MeanDouble)
 {
-    std::vector<double> vec = { 1., 2., 3., 4., 5.};
+    std::vector<double> vec = {1., 2., 3., 4., 5.};
     auto mean = vector_mean(vec);
     ASSERT_FLOAT_EQ(mean, 3.);
 };
 
-TEST(UtilsTest, TestStdDevComplex)
+TEST(UtilsTest, StdDevComplex)
 {
     std::vector<std::complex<double>> vec(5);
     for (int i = 1; i <= vec.size(); i++)
-        vec[i-1] = std::complex<double>(i, vec.size() - i);
+        vec[i - 1] = std::complex<double>(i, vec.size() - i);
     auto stddev = vector_stddev(vec);
     ASSERT_FLOAT_EQ(stddev, 2.);
 };
 
-TEST(UtilsTest, TestStdDevDouble)
+TEST(UtilsTest, StdDevDouble)
 {
-    std::vector<double> vec = { 1., 2., 3., 4., 5.};
+    std::vector<double> vec = {1., 2., 3., 4., 5.};
     auto stddev = vector_stddev(vec);
     ASSERT_FLOAT_EQ(stddev, std::sqrt(2.));
+};
+
+TEST(SatisticsTest, SNR)
+{
+    Rng random(1);
+    std::vector<double> truth(50);
+    std::vector<double> estimate(50);
+    double l2_true = 0;
+    for (int i = 0; i < truth.size(); i++)
+    {
+        truth[i] = random.normal(1.);
+        estimate[i] = truth[i];
+        l2_true += truth[i] * truth[i];
+    }
+    int rand_ind = random.uniform(truth.size());
+    estimate[rand_ind] += 1;
+
+    ASSERT_EQ(statistics::snr(truth, estimate), 10. * std::log10(l2_true));
 };
 
 int main(int argc, char **argv)
