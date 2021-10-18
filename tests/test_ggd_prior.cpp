ae975e681ddf20dcd49e668eb0de9fa29715e935
Auggie Marignier <augustin.marignier.14@ucl.ac.uk>
2021-10-18 13:41:34 +0100

    a somewhat meaningfull test

--- tests/test_ggd_prior.cpp
+++ tests/test_ggd_prior.cpp
@@ -1,4 +1,3 @@
-
 #include <stdio.h>
 #include <stdlib.h>
 #include <math.h>
@@ -23,75 +22,39 @@ TEST(TestFGGD, Values)
     prior = wavetree_prior_create_depth_full_generalised_gaussian(ndepths, va, beta, 1234);
     ASSERT_TRUE(prior != NULL);
 
-#if 0
     /*
    * Check generation works
    */
-    mean = 0.0;
-    for (i = 0; i < 1000; i++)
+    for (int level = 0; level < 3; level++)
     {
-        ck_assert(prior->sample(prior->user,
-                                1, 2, 3,
-                                5,
-                                6,
-                                0.0,
-                                &coeff) >= 0);
+        mean = 0.0;
+        for (i = 0; i < 1000; i++)
+        {
+            ASSERT_GE(prior->sample(prior->user,
+                                    1, 2, 3,
+                                    level,
+                                    3,
+                                    0.0,
+                                    &coeff),
+                      0);
 
-        ck_assert(coeff >= -1.0);
-        ck_assert(coeff <= 1.0);
+            mean = mean + coeff;
 
-        mean = mean + coeff;
+            ASSERT_GE(prior->prob(prior->user,
+                                  1, 2, 3,
+                                  level,
+                                  3,
+                                  0.0,
+                                  coeff),
+                      0.);
+        }
 
-        ck_assert(prior->prob(prior->user,
-                              1, 2, 3,
-                              5,
-                              6,
-                              0.0,
-                              coeff) == 0.5);
-
-        ck_assert(prior->valid(prior->user,
-                               1, 2, 3,
-                               5,
-                               6,
-                               0.0,
-                               coeff));
+        /*
+        * Ensure that the mean is near 0
+        */
+        mean /= (double)1000;
+        ASSERT_NEAR(fabs(mean), 0., 1e-1);
     }
-
-    /*
-   * Ensure that the mean is near 0
-   */
-    mean /= (double)1000;
-    ck_assert(fabs(mean) < 0.1);
-
-    /*
-   * Check values outside of prior return invalid an 0 prob
-   */
-    ck_assert(prior->prob(prior->user,
-                          1, 2, 3,
-                          5,
-                          6,
-                          0.0,
-                          2.0) == 0.0);
-    ck_assert(prior->valid(prior->user,
-                           1, 2, 3,
-                           5,
-                           6,
-                           0.0,
-                           2.0) == 0);
-
-    ck_assert(prior->prob(prior->user,
-                          1, 2, 3,
-                          5,
-                          6,
-                          0.0,
-                          -2.0) == 0.0);
-    ck_assert(prior->valid(prior->user,
-                           1, 2, 3,
-                           5,
-                           6,
-                           0.0,
-                           -2.0) == 0);
-#endif
 
     wavetree_prior_destroy(prior);
 }
