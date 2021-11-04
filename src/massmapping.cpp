5ab42891d547b55d1f087adaff23df596319d4ac
Auggie Marignier <augustin.marignier.14@ucl.ac.uk>
2021-11-04 12:12:52 +0000

    option to read in gamma

--- src/massmapping.cpp
+++ src/massmapping.cpp
@@ -9,9 +9,10 @@
 #include "globalprop.cpp"
 #include "logging.hpp"
 
-static char short_options[] = "i:I:M:o:x:y:t:S:s:k:B:w:v:l:h";
+static char short_options[] = "i:g:I:M:o:x:y:t:S:s:k:B:w:v:l:h";
 static struct option long_options[] = {
     {"input", required_argument, 0, 'i'},
+    {"input_gamma", required_argument, 0, 'g'},
     {"initial_model", required_argument, 0, 'I'},
     {"prior-file", required_argument, 0, 'M'},
     {"output", required_argument, 0, 'o'},
@@ -40,6 +41,7 @@ int main(int argc, char *argv[])
 {
     // Defaults
     char *input_kappa = nullptr;
+    char *input_gamma = nullptr;
     char *initial_model = nullptr;
     char *prior_file = nullptr;
     char *output_prefix = nullptr;
@@ -73,6 +75,9 @@ int main(int argc, char *argv[])
         {
         case 'i':
             input_kappa = optarg;
+            break;
+        case 'g':
+            input_gamma = optarg;
             break;
         case 'I':
             initial_model = optarg;
@@ -150,58 +155,78 @@ int main(int argc, char *argv[])
             break;
         }
     }
+    Logger::open_log(logfile);
 
     // Check files
-    if (input_kappa == NULL)
-    {
-        fprintf(stderr, "Please provide an input file\n");
-        return -1;
-    }
+    if (input_kappa == NULL & input_gamma == NULL)
+        throw ERROR("Please provide an input file\n");
+    if (input_kappa != NULL & input_gamma != NULL)
+        throw ERROR("Please provide only one of kappa or gamma file\n");
     if (prior_file == NULL)
-    {
-        fprintf(stderr, "Please provide a prior file\n");
-        return -1;
-    }
+        throw ERROR("Please provide a prior file\n");
     if (output_prefix == NULL)
-    {
-        fprintf(stderr, "Please provide an output directory\n");
-        return -1;
-    }
-
-    // Setup
-    Logger::open_log(logfile);
-    INFO("Reading in kappa map %s", input_kappa);
-    std::ifstream file(input_kappa);
-    complexvector kappa;
-    double element;
-    if (file.is_open())
-    {
-        while (file >> element)
-            kappa.emplace_back(element, 0);
-
-        if (1 << degreex * 1 << degreey != kappa.size())
-            throw ERROR("Incorrect image size");
-
-        file.close();
-    }
-    else
-    {
-        throw ERROR("File not opened %s", input_kappa);
-    }
+        throw ERROR("Please provide an output directory\n");
 
     mmobservations observations(1 << degreex, 1 << degreey, super);
-    complexvector gamma = observations.single_frequency_predictions(kappa);
+    complexvector gamma;
+    complexvector gamma_noisy;
+    std::vector<double> covariance;
 
-    const double ngal = 100.;
-    const double sidelength = 500.;
-    auto noise_tuple = add_gaussian_noise(gamma, ngal, sidelength);
-    auto gamma_noisy = std::get<0>(noise_tuple);
-    auto covariance = std::get<1>(noise_tuple);
-    observations.set_observed_data(gamma);
+    if (input_kappa != NULL)
+    {
+        INFO("Reading in kappa map %s", input_kappa);
+        std::ifstream file(input_kappa);
+        complexvector kappa;
+        double element;
+        if (file.is_open())
+        {
+            while (file >> element)
+                kappa.emplace_back(element, 0);
+
+            if (1 << degreex * 1 << degreey != kappa.size())
+                throw ERROR("Incorrect image size");
+
+            file.close();
+        }
+        else
+        {
+            throw ERROR("File not opened %s", input_kappa);
+        }
+        gamma = observations.single_frequency_predictions(kappa);
+        const double ngal = 100.;
+        const double sidelength = 500.;
+        auto noise_tuple = add_gaussian_noise(gamma, ngal, sidelength);
+        gamma_noisy = std::get<0>(noise_tuple);
+        covariance = std::get<1>(noise_tuple);
+        double data_snr = statistics::snr(gamma, gamma_noisy);
+        INFO("Input data SNR %10.6f dB", data_snr);
+    }
+    else if (input_gamma != NULL)
+    {
+        INFO("Reading in gamma data %s", input_gamma);
+        std::ifstream file(input_gamma);
+        double re, im, cov;
+        if (file.is_open())
+        {
+            while (file >> re >> im >> cov)
+            {
+                gamma_noisy.emplace_back(re, im);
+                covariance.emplace_back(cov);
+            }
+
+            if (1 << degreex * 1 << degreey != gamma_noisy.size())
+                throw ERROR("Incorrect image size");
+
+            file.close();
+        }
+        else
+        {
+            throw ERROR("File not opened %s", input_gamma);
+        }
+    }
+
+    observations.set_observed_data(gamma_noisy);
     observations.set_sigmas(covariance);
-
-    double data_snr = statistics::snr(gamma, gamma_noisy);
-    INFO("Input data SNR %10.6f dB", data_snr);
 
     GlobalProposal global(&observations,
                           initial_model,
@@ -400,7 +425,8 @@ static void usage(const char *pname)
             "usage: %s [options]\n"
             "where options is one or more of:\n"
             "\n"
-            " -i|--input <file>               Input observations file\n"
+            " -i|--input <file>               Input kappa file.  One column\n"
+            " -g|--input_gamma <file>         Input gamma file. Three columns (g1, g2, cov)\n"
             " -I|--inital_model <file>        Initial model file for restarts\n"
             " -M|--prior <file>               Prior/Proposal file\n"
             " -o|--output <path>              Output prefix for output files\n"
