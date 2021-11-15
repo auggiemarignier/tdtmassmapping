6f6baa3da6affdd99b1533f072f802911fe69535
Auggie Marignier <augustin.marignier.14@ucl.ac.uk>
2021-11-15 11:12:38 +0000

    adjoint dot test should work

--- tests/test_mmobservations.cpp
+++ tests/test_mmobservations.cpp
@@ -108,6 +108,59 @@ TEST_F(MMObsTest, KaiserSquiresAdj)
     ASSERT_FLOAT_EQ(g1dotg2.imag(), k1dotk2.imag());
 }
 
+class MMObsSuperTest : public ::testing::Test
+{
+protected:
+    void SetUp() override
+    {
+        const uint imsizex = 32;
+        const uint imsizey = 32;
+        const int super = 2;
+        observations = new mmobservations(imsizex, imsizey, super);
+        random = new Rng(1);
+    }
+    void TearDown() override
+    {
+        delete observations;
+    }
+
+    mmobservations *observations;
+    Rng *random;
+    static constexpr uint imsize = 32 * 32;
+    static constexpr uint superimsize = 64 * 64;
+};
+
+TEST_F(MMObsSuperTest, KaiserSquiresAdj)
+{
+    complexvector k1(superimsize);
+    complexvector k2(superimsize);
+    complexvector g1(imsize);
+    complexvector g2(imsize);
+    for (uint j = 0; j < superimsize; j++)
+        k1[j] = std::complex<double>(random->normal(1.), random->normal(1.));
+    for (uint j = 0; j < imsize; j++)
+        g2[j] = std::complex<double>(random->normal(1.), random->normal(1.));
+
+    observations->kaiser_squires(g1, k1);
+    observations->kaiser_squires_adj(k2, g2);
+    for (int i = 1; i < imsize; i++)
+        ASSERT_NE(std::complex<double>(0, 0), g1[i]);
+    for (int i = 1; i < superimsize; i++)
+        ASSERT_NE(std::complex<double>(0, 0), k2[i]);
+
+    std::complex<double> g1dotg2 = 0;
+    std::complex<double> k1dotk2 = 0;
+    for (uint j = 0; j < imsize; j++)
+        g1dotg2 += g1[j] * std::conj(g2[j]);
+    for (uint j = 0; j < superimsize; j++)
+        k1dotk2 += k1[j] * std::conj(k2[j]);
+
+    ASSERT_NE(std::complex<double>(0, 0), g1dotg2);
+    ASSERT_NE(std::complex<double>(0, 0), k1dotk2);
+    ASSERT_FLOAT_EQ(g1dotg2.real(), k1dotk2.real());
+    ASSERT_FLOAT_EQ(g1dotg2.imag(), k1dotk2.imag());
+}
+
 int main(int argc, char **argv)
 {
     ::testing::InitGoogleTest(&argc, argv);
